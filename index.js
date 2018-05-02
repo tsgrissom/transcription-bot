bc788a0a7cbb897d0cc1f6634c7696f2df494893
Tyler Grissom <minestein123@gmail.com>
2018-05-02 16:45:42 -0500

    Changed pastebin API calls

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const yaml = require('js-yaml');
 const PastebinAPI = require('pastebin-js');
-const pastebin = new PastebinAPI('4f8af4062138d96962bdecae16ca9d9b');
+
+let pastebinKey = '';
+let botKey = '';
+let pastebin = null;
 
 client.on('message', message => {
     if (message.content.toLowerCase() === '-transcript') {
-    message.channel.fetchMessages()
-        .then(messages => {
-        let text = "";
+        message.channel.fetchMessages()
+            .then(messages => {
+                let text = "";
 
-    for (let [key, value] of messages) {
-        let createdAt = convertTimestamp(value.createdTimestamp);
+                for (let [key, value] of messages) {
+                    text += `${value.author.tag}: ${value.content}\n`;
+                }
 
-        text += `${value.author.tag}: ${value.content}\n`;
+                console.log(text);
+
+                pastebin.createPaste({
+                        text: text,
+                        title: "Transcript",
+                        format: null,
+                        privacy: 1
+                    })
+                    .then(data => {
+                        console.log(`Created paste: ${data}`);
+
+                        message.author.send(`Transcript: ${data}`)
+                            .then(() => console.log(`Sent user "${message.author.tag}" transcript.`))
+                            .catch((err) => {
+                                console.log(`Could not PM transcript, falling back to message in channel: ${err}`);
+                                message.reply(data).fail((err) => console.log(`Uh oh! Something went wrong: ${err}`));
+                            });
+                    })
+                    .fail(err => {
+                        console.log(`Failed to create paste: ${err}`);
+                    });
+            })
+            .catch(err => {
+                console.log(`Failed to fetch messages: ${err}`);
+            });
     }
-
-    pastebin.createPaste(text, "Transcript")
-        .then(data => {
-        console.log(`Created paste: ${data}`);
-
-    message.author.send(`Transcript: ${data}`).then(() => console.log(`Sent user "${message.author.tag}" transcript.`)).catch((err) => {
-        console.log('Could not PM transcript, falling back to message in channel');
-
-    message.reply(data)
-        .fail((err) => console.log(`Uh oh! Something went wrong: ${err}`));
 });
-})
-.fail(err => console.log(err));
-})
-.catch(console.error);
 
-    if (message.deletable) {
-        message.delete();
-    } else {
-        console.log('Could not delete message. The bot needs permission to do this.');
-    }
+try {
+    config = yaml.safeLoad(fs.readFileSync('configuration.yml', 'utf8'));
+
+    pastebinKey = config.pastebinKey;
+    botKey = config.botKey;
+} catch (e) {
+    console.log(e);
 }
-});
 
-client.login('NDQxMjU4OTU0MTI3OTY2MjE5.Dctp8g.HSIW0dVmSZ4jKSVzGS2tbDdJOuU');
+client.login(config.botKey)
+    .then(() => {
+        pastebin = new PastebinAPI(pastebinKey);
+
+        console.log('Bot ready!')
+    });
